2f7d65c548c8fd0655190eaf767a8fe176a3b1d6
yanghai <yanghai0523@gmail.com>
2018-01-18 23:08:55 +0800

    添加网络请求框架

---
+++ src/yhhttp/mHttp.go
@@ -0,0 +1,35 @@
+package yhhttp
+
+import (
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"fmt"
+)
+
+func HttpRequest(method func() (*http.Request,error)) (*BaseResponse,error) {
+	client := &http.Client{}
+	req,err :=method()// GenerateReqeust(bc,param)
+	resp, err := client.Do(req)
+	if err != nil {
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+		return nil,err
+	}
+	var br BaseResponse
+	json.Unmarshal(body, &br)
+	fmt.Println(string(body))
+	//返回请求的结果
+	return &br,nil
+}
+/**
+请求接口成功后返回的信息
+*/
+type BaseResponse struct {
+	Code int
+	Msg  string
+	Data map[string]interface{}
+}
