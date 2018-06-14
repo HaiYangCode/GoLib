b5a4db0879e44becd89d1e0fa5df8938d0e74947
yanghai <yanghai0523@gmail.com>
2018-06-14 20:27:03 +0800

    fix

--- athttp/http.go
+++ athttp/http.go
@@ -7,9 +7,9 @@ import (
 	"fmt"
 )
 
-func HttpRequest(method func() (*http.Request,error)) (*BaseResponse,error) {
+func HttpRequest(method func() (*http.Request, error)) (*BaseResponse, error) {
 	client := &http.Client{}
-	req,err :=method()// GenerateReqeust(bc,param)
+	req, err := method() // GenerateReqeust(bc,param)
 	resp, err := client.Do(req)
 	if err != nil {
 	}
@@ -17,14 +17,15 @@ func HttpRequest(method func() (*http.Request,error)) (*BaseResponse,error) {
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		// handle error
-		return nil,err
+		return nil, err
 	}
 	var br BaseResponse
 	json.Unmarshal(body, &br)
 	fmt.Println(string(body))
 	//返回请求的结果
-	return &br,nil
+	return &br, nil
 }
+
 /**
 请求接口成功后返回的信息
 */
@@ -32,4 +33,22 @@ type BaseResponse struct {
 	Code int
 	Msg  string
 	Data interface{}
+}
+
+func Http0Request(method func() (*http.Request, error)) (interface{}, error) {
+	client := &http.Client{}
+	req, err := method() // GenerateReqeust(bc,param)
+	resp, err := client.Do(req)
+	if err != nil {
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+		return nil, err
+	}
+	var br interface{}
+	json.Unmarshal(body, &br)
+	//返回请求的结果
+	return &br, nil
 }
