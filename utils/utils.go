ad27a8ae64d2589246e571ebb07ed5c317098ce7
yanghai <yanghai0523@gmail.com>
2018-08-17 16:54:54 +0800

    deal with result

---
+++ utils/utils.go
@@ -0,0 +1,20 @@
+package utils
+
+
+import (
+	"net/http"
+	"io"
+	"encoding/json"
+)
+
+var result = make(map[string]interface{})
+
+func RStatus(w http.ResponseWriter, statusCode int, statusMsg string, resultData interface{}) {
+	result["code"] = statusCode
+	result["msg"] = statusMsg
+	result["data"] = resultData
+	w.Header().Set("Content-Type", "application/json")
+	data, _ := json.Marshal(result)
+	io.WriteString(w, string(data))
+}
+
