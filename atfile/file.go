0679bd74083c7d71e90b348bb5f3823724014bea
yanghai <yanghai0523@gmail.com>
2019-03-27 18:51:04 +0800

    添加新的工具

--- atfile/file.go
+++ atfile/file.go
@@ -1,9 +1,13 @@
 package atfile
 
 import (
+	"fmt"
+	"github.com/yanghai23/GoLib/atfile"
+	"io/ioutil"
 	"os"
 	"log"
 	"path/filepath"
+	"runtime"
 	"strings"
 )
 
@@ -26,4 +30,26 @@ func GetCurrentDirectory() string {
 		log.Fatal(err)
 	}
 	return strings.Replace(dir, "\\", "/", -1)
+}
+
+var ostype = runtime.GOOS
+/**
+	读取配置
+ */
+
+func ReadConfig(fileName string) (data []byte, err error) {
+	currentPath := atfile.GetCurrentDirectory()
+	fmt.Println("操作系统类型 ", ostype)
+	if "windows" == ostype {
+		data, err = ioutil.ReadFile(currentPath + "\\"+fileName)
+		fmt.Println("windows 读取配置")
+	} else {
+		data, err = ioutil.ReadFile(currentPath + "/"+fileName)
+		fmt.Println("Mac 读取配置")
+	}
+
+	if err != nil {
+		fmt.Println("err = ", err)
+	}
+	return data, err
 }
---
+++ common/param.go
@@ -0,0 +1,78 @@
+package common
+
+const (
+	SUCCESS = 200
+	FAILED  = 300
+)
+
+const (
+	QUERY_SUCCES  = "查询成功"
+	SAVE_SUCCES   = "存储成功"
+	SUBMIT_SUCCES = "反馈成功"
+)
+
+type PayStatus struct {
+	VpnId            string
+	Country          string
+	Version          string
+	DollarPrice      string
+	Type             string
+	ResultCode       string
+	Level            string
+	Code             string
+	TimeStr          string
+	Result           string
+	VpnConnectStatus string `json:"vpnConnectStatus"`
+	Status           string
+}
+
+type QueryBean struct {
+	Count int
+}
+
+type LogFile struct {
+	AccountId string
+	Date      string
+	Path      string
+	FileName  string
+}
+type ProblemInfo struct {
+	Id          int
+	AccountId   string
+	VpnId       string
+	Problem     string
+	ContactInfo string
+	Date        string
+}
+
+type Event struct {
+	Event      string `json:"event"`
+	Timestamp  string
+	TimePhone  string
+	Uuid       string
+	Androidid  string
+	PhoneType  string
+	Language   string
+	Country    string
+	AppVersion string
+	OsVersion  string
+	Segment    string
+	Level      string
+	SdkVersion string
+}
+
+type OtherParam struct {
+	OrderId          string
+	PackageName      string
+	ProductId        string
+	PurchaseTime     string
+	PurchaseState    string
+	DeveloperPayload string `json:"developerPayload"`
+	PurchaseToken    string `json:"purchaseToken"`
+}
+
+type Country struct {
+	Code string `json:"code"`
+	En   string `json:"en"`
+	Cn   string `json:"cn"`
+}
