f4626f14232f7d0178ea493938909fb9b44febb6
kk <kk@gmail.com>
2021-03-05 12:07:28 +0800

    dev： 更新库

--- atdb/mysql.go
+++ atdb/mysql.go
@@ -1,35 +1,42 @@
 package atdb
+
 import (
 	"database/sql"
-	"io/ioutil"
 	"encoding/json"
 	_ "github.com/go-sql-driver/mysql"
+	"io/ioutil"
+	"time"
 )
 
-func InitMysql(config DbConfig)(*sql.DB,error) {
+func InitMysql(config DbConfig) (*sql.DB, error) {
 	db, err := sql.Open("mysql", config.Name+":"+config.Pwd+"@tcp("+config.Ip+")/"+config.DbName+"?charset=utf8")
-	return db,err
+	// See "Important settings" section.
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	return db, err
 }
 
 /**
-	读取数据库的配置文件
- */
-func ReadDbConfig(path string) (*DbConfig,error) {
-	res,err := ioutil.ReadFile(path)
-	if err!= nil {
-		return nil,err
+读取数据库的配置文件
+*/
+func ReadDbConfig(path string) (*DbConfig, error) {
+	res, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
 	}
 	var config DbConfig
 
-	err = json.Unmarshal(res,&config)
+	err = json.Unmarshal(res, &config)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-	return &config,nil
+	return &config, nil
 }
-type DbConfig struct{
-	Name string
-	Pwd string
-	Ip string
+
+type DbConfig struct {
+	Name   string
+	Pwd    string
+	Ip     string
 	DbName string
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,6 @@
 module github.com/yanghai23/GoLib
+
+go 1.16
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+)
